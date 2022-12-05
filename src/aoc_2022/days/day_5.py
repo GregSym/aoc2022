4cabea2607198533e99647130b70ac5ad07b21a9
GregSym <gtsrex@gmail.com>
2022-12-05 20:15:58 +0200

    part 1

--- src/aoc_2022/days/day_5.py
+++ src/aoc_2022/days/day_5.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+import collections
 from dataclasses import dataclass
 import re
 from typing import Self
@@ -19,6 +21,40 @@ move 1 from 1 to 2"""
 @pytest.fixture
 def input():
     return test_input
+
+
+@dataclass
+class Stack:
+    stack: dict[int, list[str]]
+    isntrs: list[Instr]
+
+    @classmethod
+    def from_str(cls, text: str, instrs: list[Instr]) -> Self:
+        pattern = re.compile(r"(?P<index>[0-9]+)")
+        lines = text.splitlines()
+        index_locations = [
+            (match.start("index"), int(match["index"])) for match in pattern.finditer(lines.pop())
+        ]
+        stack: collections.defaultdict[int, list[str]] = collections.defaultdict(list)
+        for i, stack_num in index_locations:
+            for line in lines:
+                if line[i] != " ":
+                    stack[stack_num].append(line[i])
+        stack = {k: list(reversed(v)) for k, v in stack.items()}
+        return cls(stack, instrs)
+
+    def exec_instr(self, instr: Instr):
+        for _ in range(instr.move):
+            self.stack[instr.destination].append(self.stack[instr.current].pop())
+
+    def exec_queue(self):
+        """exec full instr queue"""
+        for instr in self.isntrs:
+            self.exec_instr(instr)
+
+    @property
+    def top_crates(self):
+        return "".join(v.pop() for v in self.stack.values() if v)
 
 
 @dataclass
@@ -48,6 +84,9 @@ class Instr:
 def solve_day(input: str) -> str:
     stack, instructions = DataTransforms(input).header_footer
     instrs = Instr.from_instructions(instructions)
+    stk = Stack.from_str(stack, instrs)
+    stk.exec_queue()
+    return stk.top_crates
 
 
 def test_day_5_part_1(input: str) -> None:
@@ -56,6 +95,5 @@ def test_day_5_part_1(input: str) -> None:
 
 if __name__ == "__main__":
     real_input = DayInterface(5).get_day()
-    print(real_input.split("\n\n"))
     test_day_5_part_1(test_input)
     print(DayInterface(5).submit_day(solve_day(real_input)))
