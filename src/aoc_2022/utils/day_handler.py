e34a8451e675bfecaaec2f69ef058cb07caa83ad
GregSym <gtsrex@gmail.com>
2022-12-06 20:22:55 +0200

    lol, forgot to index into regex match here

--- src/aoc_2022/utils/day_handler.py
+++ src/aoc_2022/utils/day_handler.py
@@ -25,16 +25,21 @@ class DayInterface:
             return f"{self.year_url}/{day}/answer"
 
         res = requests.post(
-            build_url(self.day), data={"level": part, "answer": data}, cookies={"session": self.key}
+            build_url(self.day),
+            data={"level": part, "answer": data},
+            cookies={"session": self.key},
         )
 
-        key_phrases = ["That\'s the right answer!", "Both parts of this puzzle are complete!"]
+        key_phrases = [
+            "That's the right answer!",
+            "Both parts of this puzzle are complete!",
+        ]
         for phrase in key_phrases:
             if phrase in res.text:
                 return phrase
         wait_time = re.search(r"You have (?P<secs>[0-9]+)s left to wait", res.text)
         if wait_time is not None:
             return f"""You submitted too recently. 
-            You have {wait_time}s left to wait. 
+            You have {wait_time['secs']}s left to wait. 
             (try not to spam their site)"""
         return res, res.text
