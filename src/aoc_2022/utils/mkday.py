18e07d80f373f637066396d546b95c63592b1469
GregSym <gtsrex@gmail.com>
2022-12-18 19:36:14 +0200

    template generator

--- src/aoc_2022/utils/mkday.py
+++ src/aoc_2022/utils/mkday.py
@@ -1,14 +1,17 @@
 import pathlib
 import os
+import re
 
 here = os.path.dirname(__file__)
 
 template = here / pathlib.Path("../static/template.txt")
 
-with open(template) as file:
-    txt = file.read()
 
 
 def write_file(path: str | pathlib.Path, day: int) -> None:
+    with open(template) as file:
+        txt = file.read()
+    txt = re.sub(r"DayInterface\([0-9]+\)", f"DayInterface({day})", txt)
+    txt = re.sub(r"day\_[0-9]+", f"day_{day}", txt)
     with open(path, "w") as file:
         file.write(txt)
